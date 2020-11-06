f21beb7cab1ab5686d7a7e5d91980c99ecc72c39
rafik hammas <rafik.hammas@outlook.com>
2020-11-06 14:35:59 +0100

    Spotify-clone

---
+++ src/Footer.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import ReplayIcon from "@material-ui/icons/Replay";
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/en/7/74/Usher_-_Confessions_album_cover.jpg"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__green" />
+        <SkipNextIcon className="footer__icon" />
+        <ReplayIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/reducer.js
@@ -0,0 +1,43 @@
+export const initialState = {
+  user: null,
+  playlistes: [],
+  playing: false,
+  item: null,
+  token: null,
+  //put "your-token" to skip login,
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  //action -> type, [payload]
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    default:
+      return state;
+  }
+};
+export default reducer;
